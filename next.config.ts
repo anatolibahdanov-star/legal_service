67793a1e53743d7ff789117c4290ac63c2215acd
Anatoli Bahdanau <anatoli.bahdanau.brokerd@gmail.com>
2026-01-29 16:17:46 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://lllms-dev.ru/api/:path*',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://lllms-dev.ru/api/:path*',
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
